d894b880098341e199897677832663efd5189005
AI4eve <wulogic@126.com>
2021-05-15 21:49:45 +0800

    展示每次学习的精确度变化

--- kerasStudy.py
+++ kerasStudy.py
@@ -2,6 +2,11 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Activation
 from tensorflow.keras.utils import plot_model
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+
+matplotlib.use('TkAgg')
+
 
 # 适配intel cpu
 import os
@@ -22,20 +27,38 @@ if __name__ == '__main__':
     training_data = np.random.random((training_num, 2))
     labels = [(1 if data[0] < data[1] else 0) for data in training_data]
 
-    print(training_data)
-
-    model.fit(training_data, np.array(labels), epochs=20, batch_size=32)
-
+    # print(training_data)
     test_num = 100
     test_data = np.random.random((test_num, 2))
     expected = [(1 if data[0] < data[1] else 0) for data in test_data]
 
-    error = 0
-    for i in range(0, test_num):
-        data = test_data[i].reshape(1, 2)
-        pred = 0 if model.predict(data) < 0.5 else 1
+    epoch_x = []
+    acc_y = []
 
-        if (pred != expected[i]):
-            error += 1
+    for nn in range(1, 10):
+        print(f'-第{nn}次训练-------------------------------------------------------------')
+        model.fit(training_data, np.array(labels), epochs=20 * nn, batch_size=32, verbose=0)
+        error = 0
+        for i in range(0, test_num):
+            data = test_data[i].reshape(1, 2)
+            pred = 0 if model.predict(data) < 0.5 else 1
 
-    print(f'total erros:{error}, acc:{1.0 - error / test_num}')
+            if (pred != expected[i]):
+                error += 1
+        acc = 1.0 - error / test_num
+
+        epoch_x.append(20*nn)
+        acc_y.append(acc)
+        plt.plot(20*nn, acc, 'r.-')
+        plt.pause(0.1)
+
+        print(f'total erros:{error}, acc:{acc}')
+        print('------------------------------------------------------------------\n')
+
+    print(epoch_x)
+    print(acc_y)
+
+    plt.plot(epoch_x,acc_y,'go-')
+
+    plt.pause(5)
+
